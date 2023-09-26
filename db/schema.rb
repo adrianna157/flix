a03b9dde7c8c260edb8f5a93e87ff89aac08096f
Adrianna Guevarra <adriannaestelleguevarra@gmail.com>
2023-09-25 18:17:18 -0700

    Add user signup and creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_131832) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_183244) do
   create_table "flipper_features", force: :cascade do |t|
     t.string "key", null: false
     t.datetime "created_at", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_131832) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "reviews", "movies"
