5c2b7ff379be90d3c833d638362a4b25e31b47b7
Adrianna Guevarra <adriannaestelleguevarra@gmail.com>
2023-12-25 10:35:51 -0800

    Add user authentication and enhance sign-in experience
    Implemented user authentication checks and moved 'current_user' helper method to application_controller. Additionally, now the previously visited URL is saved when a user tries to access a page requiring sign-in, improving the user experience by redirecting back to their intended destination after successful sign-in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
     if user && user.authenticate(params[:password]) # if user exists and password is correct
       session[:user_id] = user.id # set session to user's id
-      redirect_to user, notice: "Welcome back, #{user.name}!" # redirect to user's profile page
+      redirect_to (session[:intended_url] || user),
+                  notice: "Welcome back, #{user.name}!" # redirect to user's profile page
+      session[:intend_url] = nil # set session to nil
     else
       flash.now[:alert] = "Invalid email/password combination!"
       render :new, status: :unprocessable_entity
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
+
   def index
     @users = User.all
   end
@@ -22,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to @user, status: :see_other,
@@ -34,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -43,6 +45,11 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def require_correct_user
+    @user = User.find(params[:id])
+      redirect_to movies_url, alert: "Unauthorized access!" unless current_user?(@user)
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
